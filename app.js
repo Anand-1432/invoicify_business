de777d80fe9059ea1995b33d35d3c3ec1b6d94c0
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-08-12 09:57:12 +0530

    adding buld

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-const staticpath = path.join(__dirname, "./frontend/build");
+const staticpath = path.join(__dirname, "./build");
 
 console.log(staticpath);
 
