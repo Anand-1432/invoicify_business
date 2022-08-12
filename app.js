e0656a1e496cbb489dd215669343898ee0d54386
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-08-12 11:36:00 +0530

    updating

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 const express = require("express");
-const path = require('path');
 const app = express();
-
-const staticpath = path.join(__dirname, "./build");
-
-console.log(staticpath);
-
-app.use(express.static(staticpath));
 
 app.get("/",(req,res)=>{
     res.send("hellow");
