699e56b8d8576155b2bd8eb3c694fbea6b9d4fea
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-08-12 09:50:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+const staticpath = path.join(__dirname, "./frontend/build");
+
+console.log(staticpath);
+
+app.use(express.static(staticpath));
+
+app.get("/",(req,res)=>{
+    res.send("hellow");
+})
+app.get("/api",(req,res)=>{
+    res.send("hellow from api");
+})
+
+app.listen(3000);
